624108bec307d20979e5c1ef9a8af243ba072dd8
Esther Witbeck <esther.witbeck@gmail.com>
2023-08-09 09:05:37 -0700

    Set up default server-side error object

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,6 +5,38 @@ app.use(express.json());
 
 app.use(globalErrorHandler);
 
+// move to separate file
+class ErrorObj {
+  private location: string
+  private serverMsg: string
+  errorCode: number
+  private clientMsg: string
+  
+  constructor (
+    location: string,
+    serverMsg: string,
+    errorCode: number,
+    clientMsg: string
+  ) {
+    this.location = location;
+    this.serverMsg = serverMsg;
+    this.errorCode = errorCode === undefined ? 500 : errorCode;
+    if (!clientMsg) {
+      this.clientMsg = 'An unknown error occurred.'
+    } else {
+      this.clientMsg = clientMsg;
+    }
+  }
+  
+  getClientMessage(): string {
+    return this.clientMsg;
+  }
+  
+  getServerMessage(): string {
+    return `Error in ${this.location}; ${this.serverMsg}`;
+  }
+}
+
 function globalErrorHandler (
   err: Error,
   req: Request,
